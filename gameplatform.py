91e3653d4a3f2f91d6d705d9d029d83b0f7d0810
hote123456 <zhongxy@hotmail.com>
2019-06-03 15:17:31 +0800

    入口文件只在if里面执行

--- gameplatform.py
+++ gameplatform.py
@@ -3,6 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config')
 
+
 @app.route('/hello/')
 def hello():
     return 'hello word'
@@ -10,4 +11,5 @@ def hello():
 
 # app.add_url_rule('/hello', view_func=hello)
 if __name__ == '__main__':
+    # 生产环境 ngix+uwsgi
     app.run(host='0.0.0.0', debug=app.config['DEBUG'], port=81)
