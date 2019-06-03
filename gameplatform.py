63f00618de27f225373942c0fde794c669bcbfde
hote123456 <zhongxy@hotmail.com>
2019-06-03 14:52:10 +0800

    第二版本，基于类的视图（即插视图）

--- gameplatform.py
+++ gameplatform.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
 app = Flask(__name__)
-@app.route('/hello/')
-def hello():
 
+
+# @app.route('/hello/')
+def hello():
     return 'hello word'
 
+
+app.add_url_rule('/hello', view_func=hello)
 
 app.run(debug=True)
